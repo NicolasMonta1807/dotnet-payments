28fcaa969b08e0056084e83ec4cc8b0aa8e57855
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-11-09 19:17:34 -0500

    feat: adding db connection

--- PaymentsIntegration/Program.cs
+++ PaymentsIntegration/Program.cs
@@ -1,9 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using PaymentsIntegration.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+// Database connection
+var connectionString = builder.Configuration.GetConnectionString("PaymentsConnection");
+if (string.IsNullOrEmpty(connectionString))
+    throw new InvalidOperationException("Connection string 'PaymentsConnection' not found.");
+
+builder.Services.AddDbContext<PaymentsDbContext>(opt =>
+    opt.UseMySQL(connectionString)
+);
+
+// Swagger configuration
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
